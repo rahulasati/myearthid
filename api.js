357322b5bd2b831eed2f36255754c51c953983aa
Rahul Asati <rahulasati08@gmail.com>
2019-12-23 19:59:14 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,40 +1,15 @@
 import axios from 'axios';
 import openSocket from "socket.io-client";
 const api = "http://35.154.54.127:3068";
-const socket = openSocket("http://35.154.54.127:7896");
-// const api = "http://localhost:3068";
-// const socket = openSocket("http://localhost:7895");
-
-const apiKey = "YOUR Key Here"
-/**
- *
-* Ask myearth.id to register yourself as Vendor. Once Registered you will get apiKey.
-
-* Now put this apiKey in api.js file
-
-* Place api.js file  inside src folder.
-
-* Call generateReqNo and getApiKey to store it somwhere persistently for the whole session.
-
-* Call listenForServiceProviderResponse to listen the socket emit for service provider name. Pass requestno as argument
-
-* Call listenForUserData  for getting user Data once the user has authorized the request.Pass requestno as argument.
-
-* Call generateqr for getting QR code data to be scanned by mobile.
-
-*/
-
+const socket = openSocket("http://35.154.54.127:7896", {transports: ['websocket', 'polling', 'flashsocket'] });
+let socketId;
+socket.on('connect',()=>{
+    socketId = socket.id
+    console.log("Heres socketId ",socketId)
+})
+const apiKey = "BNBUQUTPSYH"
 const externalFunctions = {
-/**
- * Generates socket Id Called once and store it persistently somewhere
- * 
- * @returns socketId for communication
- */
-    async generateReqNo(){
-        var reqNo = Math.floor(100000+ Math.random() * 900000);
-        return reqNo;
-    },
-
+    
 /**
  * Returns API Key for vendor
  * 
@@ -44,55 +19,87 @@ const externalFunctions = {
     },    
 /**
  * API for generating QR Code 
- * @param {string} requestNo Socket Id for listening.
+ * @example response 
+ * qrData=`{"apikey":"werty","encryptionkey":"1234567","reqNo":"qwertyuuytr","sessionKey":"wertyuytresd" }`  
  */    
-    async  generateqr(requestNo) {
+    async  generateqr() {
         return new Promise((resolve, reject) => {
             console.log("apiKey ",apiKey)
+            try{
             if(!apiKey){
                 return new Error("apiKey not availaible")
             }
-            axios.get(`${api}/authorize/generateqr?apiKey=${apiKey}`, { crossdomain: true } )
+            if(!socketId){
+                console.log("socket not availaible try to reload page")
+                return new Error("socketId not availaible")
+            }
+            console.log("request Payload ",socketId)
+            //get session key from server and store the socketID with session key in db , to be used further during service Provider API to emit
+            axios.get(`${api}/authorize/generateqr?apiKey=${apiKey}&socketId=${socketId}`, { crossdomain: true } )
                 .then(response => {
+                    if(response.data.code === 400){
+                        let errorString = response.data.message? response.data.message:"Something went wrong"
+                        return reject(errorString);
+                    }   
                     console.log("Secret key ", response)
                     // return resolve(response.data.result.toString());
-                    let qrData =`{"apikey":"${apiKey}","encryptionkey":"1234567","reqNo":"${requestNo}","secretKey":"${response.data.result.toString()}" }`
+                    let qrData =`{"apikey":"${apiKey}","encryptionkey":"1234567","reqNo":"${response.data.result}","sessionKey":"${response.data.result}" }`                    
                     return resolve(qrData);
                 })
                 .catch(e => {
-                    console.log("This is e ",e)
+                    console.log("This is e ", e)
                     return reject("unable to get secret Token")
                 })
+            }catch(e){
+                console.log("Error in qr Code ",e)
+                return reject(e) 
+            }
         })
     },
-
 /**
  * Start Listening for service provider name
- * @param {*string} requestNo  For connecting to socket
  * @param {* function } cb callback for storing response data
+ * @example O/P Successfull
+ * {
+        "serviceProvider": "FCart ",
+    }
+    @example O/P Unsuccessfull 
+    Error object
  */
-    async  listenForServiceProviderResponse(requestNo, cb) {
-        console.log("listenForServiceProviderResponse   ",requestNo)
-        socket.on(`sendServiceProvider${requestNo}`, data => {
+    async  listenForServiceProviderResponse(cb) {
+        console.log("listenForServiceProviderResponse   ")
+        socket.on(`sendServiceProvider`, data => {
             console.log("Got Data ",data)
             cb(null, data);
         });
     },
-
 /**
  * Socket listening for user Data after approval from user through app
- * @param {*} requestNo 
- * @param {*} cb 
+ * @param {*} cb callback for data response 
+ * @returns {*function} returns a callback with newreq object
+ * @example 
+ * // Response successfull
+ * newreq:{
+ * "pressed":false,
+ * "userEmail":"srvo@gmail.com",
+ * "userMobileNo":"+916361887698",
+ * "fname":"Sarvottam",
+ * "dob":"05121993",
+ * "emailVerified":true
+ * "mobileVerified":false
+ * "score":250
+ * }
+ * //Unsuccessfull 
+ * * newreq:{
+ * "pressed":true,
+ * }
  */
-    async listenForUserData(requestNo,cb) {
-        console.log("listenForServiceProviderResponse   ",requestNo)
-
-        socket.on(`userdata${requestNo}`, data => {
+    async listenForUserData(cb) {
+        console.log("listenForServiceProviderResponse   ")
+        socket.on(`userdata`, data => {
             console.log("Got Data userdata = = =",data)
-
             cb (null,data);
         });
     }
-
 }
 export default externalFunctions
